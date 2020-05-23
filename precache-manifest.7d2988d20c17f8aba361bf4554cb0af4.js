2759c8b85048a02ea299a0e85f1cbe246bd551d2
unknown <jhunjhunwalanakul@gmail.com>
2020-05-23 18:49:16 +0530

    Updates

---
+++ precache-manifest.7d2988d20c17f8aba361bf4554cb0af4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05556310f699e5135a869fdb519c14ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a599a46dfd772b1fe8bd",
+    "url": "/static/js/2.f3bb244c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3bb244c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b33274aac15b0b831c2",
+    "url": "/static/js/main.9fe9605e.chunk.js"
+  },
+  {
+    "revision": "80624763fd9464656407",
+    "url": "/static/js/runtime-main.50aa269e.js"
+  }
+]);
