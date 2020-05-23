3d45cd4a273bef7fd315e3eeaf4b3a0fe88e85a1
unknown <jhunjhunwalanakul@gmail.com>
2020-05-23 18:49:49 +0530

    Updates

---
+++ precache-manifest.a34d7b891c73868aad3a76325d8e3526.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efaff98f4733c982a518f5f4b3e59268",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "a599a46dfd772b1fe8bd",
+    "url": "/react-weather-app/static/js/2.f3bb244c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.f3bb244c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b33274aac15b0b831c2",
+    "url": "/react-weather-app/static/js/main.9fe9605e.chunk.js"
+  },
+  {
+    "revision": "e61f36a7568d7df93152",
+    "url": "/react-weather-app/static/js/runtime-main.372fceed.js"
+  }
+]);
